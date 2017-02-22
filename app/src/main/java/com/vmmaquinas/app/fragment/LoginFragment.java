7125d351a8f4a0bf00792f96b86a3414cf71c00b
Henrique Elias <hcbe2004@gmail.com>
2017-02-22 14:03:51 -0300

    creating android project

---
+++ app/src/main/java/com/vmmaquinas/app/fragment/LoginFragment.java
@@ -0,0 +1,32 @@
+package com.vmmaquinas.app.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vmmaquinas.app.R;
+
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    public static LoginFragment newInstance() {
+        LoginFragment fragment = new LoginFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+
+}
